aca7ac6772eec1046ea59d2576495c6f9fcbf93c
MohanavelR <rmohanavel2003@gmail.com>
2025-10-13 10:28:18 +0530

    vercel

--- src/components/layout/shop/HomeShopCard.jsx
+++ src/components/layout/shop/HomeShopCard.jsx
@@ -196,7 +196,7 @@ async function handleAddToCart(id) {
             >
               {
                 isLoading && addCartProductId===product?._id ?<Loader/>:
-              <div className="flex py-1" >
+              <div className="flex py-1 space-x-3" >
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                 </svg>
